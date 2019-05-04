fdc33cf24b1745744aca5e3eca4d95df3890b6bf
Koala <bozonedouard@gmail.com>
2019-05-04 19:40:07 +0200

    Add files generation for new command

--- src/new/index.ts
+++ src/new/index.ts
@@ -1,7 +1,61 @@
-import { Rule, SchematicContext, Tree } from "@angular-devkit/schematics";
+import { strings } from "@angular-devkit/core";
+import {
+  apply,
+  applyTemplates,
+  mergeWith,
+  move,
+  Rule,
+  SchematicContext,
+  SchematicsException,
+  Tree,
+  url
+} from "@angular-devkit/schematics";
+import {
+  NodePackageInstallTask,
+  RepositoryInitializerTask
+} from "@angular-devkit/schematics/tasks";
 
-import { Schema as EffectOptions } from "./schema";
+import { Schema as NewOptions } from "./schema";
 
-export default function(_options: EffectOptions): Rule {
-  return (_host: Tree, _context: SchematicContext) => {};
+export default function(options: NewOptions): Rule {
+  if (!options.name) {
+    throw new SchematicsException(`Invalid options, "name" is required.`);
+  }
+
+  if (!options.directory) {
+    options.directory = options.name;
+  }
+
+  return (_host: Tree, context: SchematicContext) => {
+    let packageTask;
+    if (!options.skipInstall) {
+      packageTask = context.addTask(
+        new NodePackageInstallTask(options.directory)
+      );
+    }
+    if (!options.skipGit) {
+      const commit =
+        typeof options.commit == "object"
+          ? options.commit
+          : !!options.commit
+          ? {}
+          : false;
+
+      context.addTask(
+        new RepositoryInitializerTask(options.directory, commit),
+        packageTask ? [packageTask] : []
+      );
+    }
+
+    return mergeWith(
+      apply(url("./files"), [
+        applyTemplates({
+          utils: strings,
+          directory: options.directory,
+          ...options
+        }),
+        move(options.directory || options.name)
+      ])
+    );
+  };
 }
--- src/new/schema.ts
+++ src/new/schema.ts
@@ -8,4 +8,19 @@ export interface Schema {
    * The directory of the project.
    */
   directory?: string;
+
+  /**
+   * Run npm install at init.
+   */
+  skipInstall?: boolean;
+
+  /**
+   * Initialize git.
+   */
+  skipGit?: boolean;
+
+  /**
+   * Make an initial commit with generated files.
+   */
+  commit?: boolean;
 }
